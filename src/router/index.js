8583fbc2811fb00350feea8372b293e8d3b1d6a1
LiuRuiDa <liuruida999@gmail.com>
2025-04-13 20:59:40 +0800

    chore: 修改路由模式为 hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import menu from './menu'
 import setupRouterGuard from './guard'
 import Layout from '@/layout/index.vue'
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes,
 })
 
