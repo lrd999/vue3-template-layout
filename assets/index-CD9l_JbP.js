d7363544491394fbede81ea5cb280ece9b57b7da
lrd999 <liuruida999@gmail.com>
2025-04-13 12:51:49 +0000

    Deploying to gh-pages from @ lrd999/vue3-template-layout@fed1b95294f73179fa9f79e34a6c9e6ecfebfe04 🚀

--- assets/index-CD9l_JbP.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-BPB6_PxD.js";const o={},n={class:"h-full flex-center text-20"};function s(r,_){return t(),c("div",n,"Welcome")}const l=e(o,[["render",s]]);export{l as default};
